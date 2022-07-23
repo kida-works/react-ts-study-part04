9c7edae37b3568eca0c2c7ff3d3bb427b845efa6
kida-works <kida.works.1234@gmail.com>
2022-07-23 23:22:44 +0900

    End of review of useEffect.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
+import CleanUp from "./CleanUp";
 
 const App: React.FC = () => {
   const [status, setStatus] = useState<string | number>("text");
   const [input, setInput] = useState<string>("");
+  const [counter, setCounter] = useState<number>(0);
+  const [display, setDisplay] = useState<boolean>(true);
+
   const onChengeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInput(e.target.value);
   };
+
+  // useEffectで第二引数をどうするかで実行タイミングを決められる。
+  // 何もない場合、useStateの更新があるたびに実行される
+  // []のからの場合、初回の一回だけ更新される
+  // 値を入れた場合、肺あtが変更されたときに実行される
+  useEffect(() => {
+    console.log("useEffect in App invoked !");
+    document.title = `current value is ${counter}`;
+  }, [counter]);
   return (
     <div className="App">
       <header className="App-header">
@@ -15,6 +28,12 @@ const App: React.FC = () => {
         <button onClick={() => setStatus("new text")}>Button</button>
         <h4>{input}</h4>
         <input type="text" value={input} onChange={onChengeHandler} />
+        <h4>{counter}</h4>
+        <button onClick={() => setCounter((preCounter) => preCounter + 1)}>
+          Increment
+        </button>
+        {display && <CleanUp />}
+        <button onClick={() => setDisplay(!display)}>Toggle Display</button>
         <Counter />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
